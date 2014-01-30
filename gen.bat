f76bf06d6cc89e28845bc1b293ce9af83999ed60
Jeff Yutzler <jyutzler@yahoo.com>
2014-01-30 15:00:02 -0500

    Adding explicit 1NT support

--- gen.bat
+++ gen.bat
@@ -17,8 +17,8 @@ echo ## 2NT and 3NT >> system.md
 type NotrumpOpenings\2NT+3NT\SAYC.md >> system.md
 echo # Major Openings >> system.md
 type MajorOpenings\SAYC.md >> system.md
-echo ## Responses >> system.md
 type MajorOpenings\Basic\forcing2NT.md >> system.md
+type MajorOpenings\1NT\SAYC.md >> system.md
 echo # Minor Openings >> system.md
 type MinorOpenings\SAYC.md >> system.md
 echo ## Rebids by Opener >> system.md
