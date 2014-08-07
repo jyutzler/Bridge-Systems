5884460b94f59ecb4a95ac189883038c9152a830
Jeff Yutzler <jeffy@imagemattersllc.com>
2014-08-07 00:09:30 -0400

    Merge

--- gen.bat
+++ gen.bat
@@ -47,8 +47,8 @@ type Slam\gerber.md >> system.md
 echo ## Defensive Bidding >> system.md
 type Defensive\Overcall\SAYC.md >> system.md
 type Defensive\NTOvercall\systems-on.md >> system.md
-type Defensive\UnusualNT\SAYC.md >> system.md
-type Defensive\Michaels\SAYC.md >> system.md
+type Defensive\UnusualNT\RP.md >> system.md
+type Defensive\Michaels\RP.md >> system.md
 type Defensive\Doubles\SAYC.md >> system.md
 type Preemptive\sound.md >> system.md
 echo ## Competitive Bidding >> system.md
